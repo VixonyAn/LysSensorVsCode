b0a1178a654596cc0096736896e4efaa83f136d1
Magnus Sverdrup <magnus_s@live.dk>
2025-11-27 18:38:58 +0100

    UDP Sender ready

--- Python/updLysSensorSender.py
+++ Python/updLysSensorSender.py
@@ -10,82 +10,57 @@ PORT = 32000
 socket_sender = socket(AF_INET, SOCK_DGRAM)
 socket_sender.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
 
-clicked_sun = False
-curtainDrawn = False
-lightOn = False
-    
 def determineLightLevel(hour):
-    if hour >= datetime(hour=0) and hour < datetime(hour=1):
-        return random.randint(0.0001, 0.0011)
-    elif hour >= datetime(hour=1) and hour < datetime(hour=2):
-        return random.randint(0.0001, 0.0011)
-    elif hour >= datetime(hour=2) and hour < datetime(hour=3):
-        return random.randint(0.0001, 0.0011)
-    elif hour >= datetime(hour=3) and hour < datetime(hour=4):
-        return random.randint(0.0001, 0.0011)
+    if hour >= datetime(hour=0) and hour < datetime(hour=4):
+        return random.uniform(0.0001, 0.0011)
     elif hour >= datetime(hour=4) and hour < datetime(hour=5):
-        return random.randint(0.0001, 0.0108)
+        return random.uniform(0.0011, 0.0108)
     elif hour >= datetime(hour=5) and hour < datetime(hour=6):
-        return random.randint(1.08, 10.8)
+        return random.uniform(1.08, 10.8)
     elif hour >= datetime(hour=6) and hour < datetime(hour=7):
-        return random.randint(107, 1075)
+        return random.random(107, 1075)
     elif hour >= datetime(hour=7) and hour < datetime(hour=8):
         return random.randint(1075, 10752)
-    elif hour >= datetime(hour=8) and hour < datetime(hour=9):
+    elif hour >= datetime(hour=8) and hour < datetime(hour=10):
         return random.randint(10752, 50000)
-    elif hour >= datetime(hour=9) and hour < datetime(hour=10):
-        return random.randint(10752, 50000)
-    elif hour >= datetime(hour=10) and hour < datetime(hour=11):
-        return random.randint(50000, 107527)
-    elif hour >= datetime(hour=11) and hour < datetime(hour=12):
+    elif hour >= datetime(hour=10) and hour < datetime(hour=12):
         return random.randint(50000, 107527)
     elif hour >= datetime(hour=12) and hour < datetime(hour=13):
         return random.randint(107527, 120000)
-    elif hour >= datetime(hour=13) and hour < datetime(hour=14):
+    elif hour >= datetime(hour=13) and hour < datetime(hour=15):
         return random.randint(50000, 107527)
-    elif hour >= datetime(hour=14) and hour < datetime(hour=15):
-        return random.randint(50000, 107527)
-    elif hour >= datetime(hour=15) and hour < datetime(hour=16):
-        return random.randint(10752, 50000)
-    elif hour >= datetime(hour=16) and hour < datetime(hour=17):
+    elif hour >= datetime(hour=15) and hour < datetime(hour=17):
         return random.randint(10752, 50000)
     elif hour >= datetime(hour=17) and hour < datetime(hour=18):
         return random.randint(1075, 10752)
     elif hour >= datetime(hour=18) and hour < datetime(hour=19):
         return random.randint(107, 1075)
     elif hour >= datetime(hour=19) and hour < datetime(hour=20):
-        return random.randint(10.8, 107)
+        return random.uniform(10.8, 107)
     elif hour >= datetime(hour=20) and hour < datetime(hour=21):
-        return random.randint(1.08, 10.8)
+        return random.uniform(1.08, 10.8)
     elif hour >= datetime(hour=21) and hour < datetime(hour=22):
-        return random.randint(21, 40)
+        return random.uniform(1.08, 10.8)
     elif hour >= datetime(hour=22) and hour < datetime(hour=23):
-        return random.randint(0.108, 1.08)
+        return random.uniform(0.0108, 0.108)
     elif hour >= datetime(hour=23) and hour < datetime(hour=0):
-        return random.randint(0.0108, 0.108)
-
-def DrawCurtain():
-    if not curtainDrawn:
-        curtainDrawn = True
-    else:
-        curtainDrawn = False
-        
-def SwitchLight():
-    if not lightOn:
-        lightOn = True
-    else:
-        lightOn = False
-        
-def clickedNight():
-    lightOn = False
-    curtainDrawn = True
+        return random.uniform(0.0011, 0.0108)
 
 def generateDatapoint():
     date = datetime.now()
     light_dict = {
         "Date": date,
-        "IsDrawn": curtainDrawn,
-        "LightsOn": lightOn,
+        "IsDrawn": False,
+        "LightsOn": False,
         "LightLevel": determineLightLevel(date.hour)
     }
     return light_dict
+
+for _ in range(100):
+    light_dict = generateDatapoint()
+    message = json.dumps(light_dict)
+    print(f'Broadcaster sending: {message}')
+    socket_sender.sendto(message.encode(), (BROADCAST_IP, PORT))
+    time.sleep(2)
+    
+socket_sender.close()
