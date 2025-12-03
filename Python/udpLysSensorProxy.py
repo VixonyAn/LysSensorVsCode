2f53d2926f3f301f174dc48e303cef2d9a851e4b
Magnus Sverdrup <magger254@gmail.com>
2025-12-03 13:13:54 +0100

    File name correction

---
+++ Python/udpLysSensorProxy.py
@@ -0,0 +1,24 @@
+from socket import *
+import requests
+import json
+import time
+
+PORT = 32000
+
+socket_reciever = socket(AF_INET, SOCK_DGRAM)
+socket_reciever.bind(('', PORT))
+
+print("Proxy UDP ready")
+print(f"Listening for incoming messages on PORT {PORT}")
+
+REST_API_URL = "http://localhost:7169/api/" ## Include localhost number
+## REST_API_URL = "http://" ## Include Azure host url here
+
+while True:
+    msg, addr = socket_reciever.recvfrom(3000)
+    msg_str = msg.decode()
+    print(f"UDP Broadcaster {addr} sent the following message: {msg_str}")
+    msg_obj = json.loads(msg_str)
+    msg_obj["TimeTurnedOn"] = int(time.time() * 1000)
+    response = requests.post(REST_API_URL, json=msg_obj)
+    print(f"Response from REST API: {response.status_code} -- {response.text}")
